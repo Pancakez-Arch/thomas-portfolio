05ee2e76bc77c64cc00ade3aed441c2ee5496a1b
Ad-#1-am <thedarkgamer256@gmail.com>
2024-11-27 08:51:53 +0100

    Adding Front End

--- common/queries/pages/homepage.queries.ts
+++ common/queries/pages/homepage.queries.ts
@@ -10,4 +10,10 @@ export const HOMEPAGE_QUERY = groq`*[_type == "home"][0] {
         "url": asset -> url,
         alt,
     },
+    text {
+        "" : _type == "block" => @,
+        "h1": _type == "heading" && level == 1 => @,
+        "h2": _type == "heading" && level == 2 => @,
+        "h3": _type == "heading" && level == 3 => @,
+        "h4": _type == "heading" && level == 4 => @,}
 }`
